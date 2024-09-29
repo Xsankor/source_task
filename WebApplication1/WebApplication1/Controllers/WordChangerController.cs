4f34d8f4ddffe5af9ad1d20e1dbd52aed45f7b92
Xsankor <darkpolzovatel@gmail.com>
2024-09-29 19:59:57 +0500

    task_9

---
+++ WebApplication1/WebApplication1/Controllers/WordChangerController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/WordChanger")]
+    public class WordChangerController : ControllerBase
+    {
+        private readonly WordChanger wordChanger;
+
+        public WordChangerController(ConfigurationManager config) 
+        {
+            wordChanger = new WordChanger(config);
+        }
+
+        [HttpGet(Name = "GetWordChanger")]
+        public async Task<ActionResult> Get(string text, int sortedChoice = 1)
+        {
+            if (!wordChanger.CheckLine(text)) 
+            {
+                return BadRequest($"Строка может состоять только из следующих символов: {wordChanger.settingSection.ApproveSymbols}");
+            }
+
+            if (!wordChanger.CheckBlackList(text)) 
+            {
+                string[] blackList = wordChanger.settingSection.BlackList?.Select(word => word).ToArray() ?? new string[] { "" };
+                string blackListInString = string.Join(", ", blackList);
+                return BadRequest($"Строка не может соответствовать следующим словам: {blackListInString}");
+            }
+
+            string reverseWord = wordChanger.ReverseString(text);
+            Dictionary<char, int> countSymb = wordChanger.SymbCounter(text);
+            string subStr = wordChanger.FindAndPringSubstring(reverseWord);
+            string sortedString = wordChanger.SortStringAndPrint(reverseWord, sortedChoice);
+            string strWithoutRandSymbol = await wordChanger.GetRandomNumberAndRemoveChar(reverseWord);
+
+            object result = new
+            {
+                reverseWord = reverseWord,
+                countSymb = countSymb,
+                subStr = subStr,
+                sortedString = sortedString,
+                strWithoutRandSymbol = strWithoutRandSymbol,
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Tree.cs
@@ -0,0 +1,60 @@
+﻿namespace WebApplication1
+{
+    public class Tree
+    {
+        public Node? root { get; private set; }
+        private string resultStr;
+
+        public Tree(string text)
+        {
+            resultStr = string.Empty;
+
+            foreach (char chr in text)
+            {
+                root = InsertValue(root, chr);
+            }
+        }
+
+        private Node InsertValue(Node? node, char chr)
+        {
+            if (node == null)
+            {
+                node = new Node(chr);
+            }
+            else if (node.value <= chr)
+            {
+                node.left = InsertValue(node.left, chr);
+            }
+            else
+            {
+                node.right = InsertValue(node.right, chr);
+            }
+
+            return node;
+        }
+
+        public string GetSortedResult(Node? node)
+        {
+            if (node != null)
+            {
+                GetSortedResult(node.left);
+                resultStr = node.value + resultStr;
+                GetSortedResult(node.right);
+            }
+
+            return resultStr;
+        }
+    }
+
+    public class Node
+    {
+        public char value;
+        public Node? left, right;
+        public Node(char value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
